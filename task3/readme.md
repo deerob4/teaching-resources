924e8dc44c41748a8f6b2794cae64ca5dff79962
Dee Roberts <deerob4@gmail.com>
2015-11-16 21:38:09 +0000

    Build out task 3.

--- task3/readme.md
+++ task3/readme.md
@@ -2,10 +2,11 @@ Task 3 - Querying the Database
 ==============================
 Now extend your program so that it returns the films in a certain manner, based on the selected query. You should implement the following queries:
 
-* Films in alphabetical order, descending (A - Z)
+* Films in alphabetical order by title, descending (A - Z)
 * All films in a certain genre
 * Films in order of rating (descending)
 * The average (mean) of all ratings
+* The highest ranking film in each genre
 
 You should display a list of these options at the start of the program, and allow the user to choose which one they want. There should still be the option to add films. The program should follow this general flow:
 
@@ -16,6 +17,7 @@ You should display a list of these options at the start of the program, and allo
     2. Films in a certain genre
     3. Films in order of rating
     4. Average of all ratings
+    5. Higest scoring film in each genre
 
     2
 
---
+++ task3/task3.py
@@ -0,0 +1,72 @@
+import sqlite3 as lite
+
+
+def run_query(query):
+    con = lite.connect('films.db')
+
+    with con:
+        cur = con.cursor()
+        cur.execute(query)
+
+        return cur.fetchall()
+
+
+def input_films(quantity):
+    for x in range (quantity):
+        print('\nFilm #{0}:'.format(x + 1))
+        print('--------')
+
+        run_query('INSERT INTO films (title, genre, director, rating) VALUES ("{0}", "{1}", "{2}", "{3}")'.format(
+            input('What is the film called? '),
+            input('What genre does the film belong to? '),
+            input('Who directed the film? '),
+            int(input('How would you rate the film out of 10? '))
+        ))
+
+    find_films('alphabetical')
+
+
+def find_films(method):
+    if method == '1':
+        display_films(run_query('SELECT * FROM films ORDER BY title ASC'))
+
+    elif method == '2':
+        display_films(run_query('SELECT * FROM films ORDER BY rating'))
+
+    elif method == '3':
+        genre = input('Which genre do you want to view? ')
+        display_films(run_query('SELECT * FROM films WHERE genre = "{0}"'.format(genre)))
+
+    elif method == '4':
+        average = run_query('SELECT AVG(rating) FROM films')
+
+        print('The average rating of all your films is {0}'.format(average[0][0]))
+
+    elif method == '5':
+        frequencies = []
+        for x in range(10):
+            freq = run_query('SELECT COUNT(*) FROM films WHERE rating = "{0}"'.format(x))
+            frequencies.append(freq)
+
+        print(frequencies)
+
+
+def display_films(films):
+    print('\nYour films:\n')
+
+    for film in films:
+        print('Title:', film[1])
+        print('Genre:', film[2])
+        print('Director:', film[3])
+        print('Rating:', str(film[4]) + '\n')
+
+
+if __name__ == '__main__':
+    while True:
+        task = input('Do you want to add films(1) or view them(2)? ')
+        if task == '1':
+            quantity = int(input('\nHow many films do you want to enter? '))
+            input_films(quantity)
+        elif task == '2':
+            method = input('\nHow do you want to view the films? There are five ways:\n1. Alphabetically\n2. By highest rating\n3. By genre\n4. Average rating\n5. Highest rating in genre.\n')
+            find_films(method)
